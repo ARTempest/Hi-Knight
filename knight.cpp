da549a55bf5d460de6d3ffd1797a7a919234678f
ARTempest <artempest.gamer@gmail.com>
2025-01-12 00:20:58 -0500

    Fixed all the bugs and added random quote selection
    Fixed tons of bugs and added the functionality of getting quotes from a 'quotes.txt' file located in the same directory

--- knight.cpp
+++ knight.cpp
@@ -1,113 +1,116 @@
 #include <iostream>
 #include <string>
-#include <fstream>
 #include <random>
+#include <fstream>
 
 using namespace std;
 
-const int lineLength = 42;
+string quote = "I'm a coding knight";
 
-string quote = "Btw I'm a knight that prints randomly pre-generated messages.I'm gonna serve you for making your day a better thing";
+const int amountLines = 5;
+const int minimunLineLength = 42;
 
-int stringLength = quote.length();
+int quoteLength = quote.length();
+int lineLength = minimunLineLength;
+
+const int amountCharactersLimit = minimunLineLength * amountLines; // This is the limit for the normal amount of characters with a limit of 42
+
+string line[amountLines];
+
+void organizeQuote(){
+	int position = 0;
+	int beforeSpace = 0;
+	int nextEdge = lineLength;
 
 
-string line1 = "                                          ";
-string line2 = "                                          ";
-string line3 = "                                          ";
-string line4 = "                                          ";
-string line5 = "                                          ";
+	for (int i=0; i < quoteLength; i++){
+		if (quote[i] == ' '){
+			beforeSpace = i;
+		}
 
-string line[5] {line1,line2,line3,line4,line5};
+		if (beforeSpace < i && i == nextEdge){
+			
+			string spaces (i - beforeSpace - 1,' ');
 
-int calculateRest(int a, int b){
-	if ((a - b) < 0){
-		return 0;
-	}
+			quote = quote.substr(position, beforeSpace - position) + spaces + quote.substr(beforeSpace, 1000);
 
-	return (a - b);
-}
-
-string fixQuote(){
-	string newQuote = quote;
-	for (int i=0; i < (stringLength / lineLength); i++){
-		int start = lineLength * (i+1);
-
-		if (newQuote[start] != ' '){
-			while (newQuote[start - 1] != ' '){
-				start--;
-			}
-
-			string rest(lineLength*(i+1) - start,' ');
-	
-			newQuote = newQuote.substr(0,start)+rest+newQuote.substr(start,stringLength);
-		} 
-		else{
-			newQuote = newQuote.substr(0,start)+newQuote.substr(start + 1,stringLength);
 		}
 		
+		
+			
+		if (i == nextEdge && quote[i] == ' ') {
+			
+			quote = quote.substr(0, i) + quote.substr(i+1,1000);
+		}
 
-	}
-	stringLength = newQuote.length();
-	return newQuote;
-
-}
-
-void createLines(){
-	int restChar = stringLength;
-
-	for (int i = 0; i <= (stringLength / lineLength); i++){
-
-		string rest(calculateRest(lineLength, restChar),' '); // calculate the amount of ' ' needed for this line
-	
-		line[i] = quote.substr((stringLength - restChar), lineLength)+rest;
-
-		restChar = calculateRest(restChar,lineLength); // substract the length of a line from the restant characters returning the actual rest of characters that haven't been procceseated 
-	}
-}
-
-string selectQuote(){
-	int amountLines = 0;
-
-	string line;
-	ifstream myQuotes1 ("quotes.txt");
-		if (myQuotes1.is_open()){
-			while (getline(myQuotes1,line)){
-				amountLines++;
-			}
-			myQuotes1.close();
+		if (i > nextEdge){
+			nextEdge += lineLength;
 		}
 
 
+	}
+	
+	quoteLength = quote.length();
+	string extraSpaces (lineLength * amountLines - quoteLength, ' ');
+	quote = quote + extraSpaces;
+}
+
+void distributeQuote(){
+	int from = 0;
+	int to = lineLength;
+
+	for (int i=0; i < amountLines; i++){
+		line[i] = quote.substr(from, to);
+		from += lineLength;
+	}
+}
+
+int calculateAmountQuotes(){
+	string line = " ";
+	ifstream quotes ("quotes.txt");
+	int amountQuotes = 0;
+
+	if (quotes.is_open()){
+		while (getline(quotes,line)){
+			amountQuotes += 1;
+		}
+	}
+
+	quotes.close();
+
+	return amountQuotes;
+
+}
+
+int generateNumber(int amountQuotes){
 	random_device rd;
 	mt19937 gen(rd());
-	uniform_int_distribution<> distr(1,amountLines);
+	uniform_int_distribution<> distr(1, amountQuotes);
+	int number = distr(gen);
+
+	return number;
+}
+
+string selectQuote(int position){
+	string line = " ";
+	ifstream quotes ("quotes.txt");
 
 
-
-
-	ifstream myQuotes ("quotes.txt");
-
-	if (myQuotes.is_open()){
-	
-
-
-		for (int i=0; i < distr(gen); i++){
-			getline(myQuotes,line);
+	if (quotes.is_open()){
+		while (position > 0){
+			getline(quotes,line);
+			position --;
 		}
-		myQuotes.close();
 	}
+
+	quotes.close();
+
 	return line;
 }
 
 
 
-int main(){
-quote = selectQuote();
-selectQuote();
-quote = fixQuote();
-createLines();
-
+void printKnight(){
 cout << "                          ____________________________________________" << '\n';
 cout << "                          |" << line[0] << "|" << '\n';
 cout << "                          |" << line[1] << "|" << '\n';
@@ -127,13 +130,27 @@ cout << "      |   `""`  /   ^^  |   ':'   |" << '\n';
 cout << "      |       |       \\    :    /" << '\n';
 cout << "      |       |        \\   :   /"  << '\n';
 cout << "      |       |___/\\___|`-.:.-`" << '\n';
-cout << "      |        \\_ || _/"     << '\n';
+cout << "      |       \\_ || _/"     << '\n';
 cout << "      |        <_ >< _>" << '\n';
 cout << "      |        |  ||  |" << '\n';
 cout << "      |        |  ||  |" << '\n';
 cout << "      |       _\\.:||:./_" << '\n';
 cout << "      |      /____/\\____\\" << '\n';
+}
 
+int main(){
+	quote = selectQuote(generateNumber(calculateAmountQuotes()));
+	quoteLength = quote.length();
+	
+	if (quoteLength > amountCharactersLimit - 15){
+		lineLength = quoteLength / amountLines + 10;
+	}
 
-return 0;
+	organizeQuote();
+
+	distributeQuote();
+
+	printKnight();
+
+	return 0;
 }
